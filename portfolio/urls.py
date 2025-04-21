9cfd28fe907931f892c4f8aa9549e0ec655b9e38
Asparaghost <leimeandyou@gmail.com>
2025-04-21 16:54:14 +0800

    added django blocks

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('works', views.works, name='works'),
+    path('works/<int:proj_id>/',views.proj_info, name='proj_info'),
 ]
